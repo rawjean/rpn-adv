03b8a01a9f97f563b4fd0a7e81f89913dafc32a3
razeen <razeen@umich.edu>
2018-10-26 11:18:05 -0400

    initial commit

---
+++ rpn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+	# stack for calculator
+	stack = []
+
+	# tokenize input
+	tokens = arg.split()
+
+	# process tokens
+	for token in tokens:
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			val1 = stack.pop()
+			val2 = stack.pop()
+			result = val1 + val2
+			stack.append(result)
+
+
+def main():
+	while True:
+		calculate(input('rpn calc> '))
