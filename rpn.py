b87541c5f2a0e17c4389f60d977d32d9bcbb3cd6
razeen <razeen@umich.edu>
2018-11-07 03:45:35 -0500

    added color support for negative numbers

--- rpn.py
+++ rpn.py
@@ -2,6 +2,8 @@
 
 import operator
 import readline
+import colored
+from colored import fg, bg, attr
 
 
 operators = {
@@ -32,7 +34,10 @@ def calculate(myarg):
 def main():
     while True:
         result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        if result < 0:
+            print ("('Result: '," + "\033[91m" + '{0}'.format(result) + "\033[0m" + ")")
+        else:
+            print("Result: ", result)
 
 if __name__ == '__main__':
     main()
